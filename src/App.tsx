559147aa5bcc8046642d257a475b1ac536b63b2b
hakuna <943383098@qq.com>
2022-03-14 13:41:14 +0800

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,22 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { Layout } from 'antd'
+import 'antd/dist/antd.css';
+
+const { Header, Footer, Sider, Content } = Layout
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Layout style={{ height: '100vh' }}>
+      <Sider>Sider</Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
   )
 }
 
